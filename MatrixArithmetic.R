3b0f2b0e95e4b9a354836a3cf0edb158b090535c
Thi Le (TAU) <hien.le@tuni.fi>
2020-10-30 15:07:42 +0200

    Matrix arithemetic calculation

---
+++ MatrixArithmetic.R
@@ -0,0 +1,17 @@
+#Matrix Arithemetic
+mat <- matrix(1:25, byrow = T, nrow = 5)
+mat
+
+mat/2
+mat**2
+1/mat
+
+mat > 15
+mat[mat > 15]
+mat + mat
+mat/mat
+mat**mat
+mat*mat
+
+mat * mat
+mat %*% mat
