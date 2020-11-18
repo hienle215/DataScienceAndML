a065dd9e5679140d648c87fa744c40ebb6fb52e5
Thi Le (TAU) <hien.le@tuni.fi>
2020-11-18 11:05:27 +0200

    Data input and output as CSV files in R

---
+++ CSVFileWithR.R
@@ -0,0 +1,11 @@
+# CSV Files
+# Creating the CSV files
+write.csv(mtcars, files = "mysample.csv")
+
+#Reading the CSV files
+ex <- read.csv("example.csv")
+head(mtcars)
+
+#Writing
+write.csv(ex,file = "my_new_example.csv")
+read.csv("myexample.csv")
