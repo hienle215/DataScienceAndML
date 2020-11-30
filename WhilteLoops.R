a45c848676532c7c514d2fdebd16b6106173264d
Thi Le (TAU) <hien.le@tuni.fi>
2020-11-30 23:21:07 +0200

    While loops and if funtions in R

---
+++ WhilteLoops.R
@@ -0,0 +1,29 @@
+x <- 0
+while(x <10){
+  print(paste0("x is:", x))
+  x <- x+1
+}
+
+x <- 0
+while(x <10){
+  print(paste0("x is:", x))
+  
+  x <- x+1
+  if(x==10){
+    print("x is now equal to 10!Break Loop!")
+    break
+    print("Woo I printed too!")
+  }
+}
+ 
+x <- 0
+while(x <10){
+  print(paste0("x is:", x))
+  
+  x <- x+1
+  if(x==5){
+    print("x is now equal to 5!Break Loop!")
+    break
+    print("Woo I printed too!")
+  }
+}
