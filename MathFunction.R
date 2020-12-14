599abd25d4c4357f7245a67dd1159b856b361b2e
Thi Le (TAU) <hien.le@tuni.fi>
2020-12-14 14:57:51 +0200

    THe math function in R using sum, mean, round, etc

---
+++ MathFunction.R
@@ -0,0 +1,19 @@
+# Math functions with R
+
+#
+# abs() computes the absolute value
+# sum() return the sum of all the values present in the input
+# mean() computes the arithmetic mean
+#round() rounds values (additional arguments ot nearest)
+
+abs(2)
+abs(-2)
+v <- c(-2, -3, 0, 4)
+abs(v)
+sum(v)
+mean(v)
+round(v)
+round(2.333123421, digits = 2)
+round(2.37777,3)
+
+# Should dowload the reference card from internet for understanding the meaning of each function
